629ba9e296113ee67b96ed10e09548e1f74c105c
Philippe Spino <Philippe.spino@usherbrooke.ca>
2017-06-05 22:35:35 -0400

    Finished Simulink Simulation Blender

--- Script/BallBehaviour.py
+++ Script/BallBehaviour.py
@@ -8,21 +8,17 @@ scene = bpy.context.scene
 scene.frame_end = 1000
 
 sphere = bpy.data.objects['Sphere']
+bpy.ops.rigidbody.objects_add(type='ACTIVE')
 sphere.select = True
 
 sphere.rigid_body.kinematic = True
+sphere.keyframe_insert("rigid_body.kinematic",frame=1)
 
 sphere.location = (0,0,10)
 sphere.keyframe_insert(data_path="location", frame=1)
 
-x = 0.5
-y = 8.056
-sphere.location = (0,x,y)
+sphere.location = (0,0.25,9.097)
 sphere.keyframe_insert(data_path="location", frame=4)
 
-x = 1.25
-y = 4.097
-sphere.location = (0,x,y)
-sphere.keyframe_insert(data_path="location", frame=20)
-
-sphere.rigid_body.kinematic = False
+sphere.rigid_body.kinematic=False
+sphere.keyframe_insert("rigid_body.kinematic",frame=4)
