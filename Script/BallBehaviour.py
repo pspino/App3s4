0341b8cd96fcfe8c7524c13ca9cc824914e7c4b5
Philippe Spino <Philippe.spino@usherbrooke.ca>
2017-06-05 20:19:11 -0400

    Remplacer de figure.matlabfigure à JPEG

---
+++ Script/BallBehaviour.py
@@ -0,0 +1,31 @@
+import bpy
+import math
+import os
+
+os.system("cls")
+
+scene = bpy.context.scene
+scene.frame_end = 1000
+
+sphere = bpy.data.objects['Sphere']
+sphere.select = True
+
+sphere.location.x = 0
+sphere.location.y = 0
+sphere.location.z = 10
+x = sphere.location.x
+y = sphere.location.z
+g = -5
+vy = -5
+vx = 1.5
+
+rayon = 1.5
+sol = 1.2
+
+for frameNum in range(scene.frame_end + 1):
+    scene.frame_set(frameNum)
+    x = vx*(frameNum/60) 
+    y += g*(x/vx) + vy
+    sphere.location = (0,x,y)
+    sphere.keyframe_insert(data_path="location", index=-1)
+    print((x,y))
