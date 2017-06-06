4ef33ca1c95abf9bbc6e02dad3ef156c5a35c778
Philippe Spino <Philippe.spino@usherbrooke.ca>
2017-06-05 21:19:23 -0400

    update the script

--- Script/BallBehaviour.py
+++ Script/BallBehaviour.py
@@ -10,22 +10,19 @@ scene.frame_end = 1000
 sphere = bpy.data.objects['Sphere']
 sphere.select = True
 
-sphere.location.x = 0
-sphere.location.y = 0
-sphere.location.z = 10
-x = sphere.location.x
-y = sphere.location.z
-g = -5
-vy = -5
-vx = 1.5
+sphere.rigid_body.kinematic = True
 
-rayon = 1.5
-sol = 1.2
+sphere.location = (0,0,10)
+sphere.keyframe_insert(data_path="location", frame=1)
 
-for frameNum in range(scene.frame_end + 1):
-    scene.frame_set(frameNum)
-    x = vx*(frameNum/24) 
-    y = (g/2)*(x/vx)**2 + vy*(x/vx) + 10
-    sphere.location = (0,x,y)
-    sphere.keyframe_insert(data_path="location", index=-1)
-    print((x,y))
+x = 0.5
+y = 8.056
+sphere.location = (0,x,y)
+sphere.keyframe_insert(data_path="location", frame=4)
+
+x = 1.25
+y = 4.097
+sphere.location = (0,x,y)
+sphere.keyframe_insert(data_path="location", frame=20)
+
+sphere.rigid_body.kinematic = False
